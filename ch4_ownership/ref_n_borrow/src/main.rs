13e02b07b5175fde698740f11c7266d310b46bd7
agungatd <agungatidhira@gmail.com>
2024-04-02 11:37:23 +0700

    [ch4] - add some details in mutable reference

--- ch4_ownership/ref_n_borrow/src/main.rs
+++ ch4_ownership/ref_n_borrow/src/main.rs
@@ -19,7 +19,16 @@ fn fun2() {
     // Mutable references provide unique and non-owning access to data.
     let mut v: Vec<i32> = vec![1, 2, 3];
     let num: &mut i32 = &mut v[2];
-    *num += 1;
-    println!("Third element is {}", *num);
-    println!("Vector is now {:?}", v);
+
+    // println!("Vector is now {:?}", v); cannot borrow `v` as immutable 
+    //                                    because it is also borrowed as mutable
+
+    // Mutable references can also be temporarily "downgraded" to read-only references
+    let num2: &i32 = &*num;
+    println!("*num={} *num2={}", *num, *num2); // 3 3
+
+    *num += 1; // from 3 + 1 become 4 (*num)
+
+    println!("Third element is {}", *num); // 4
+    println!("Vector is now {:?}", v); // [1, 2, 4]
 }
